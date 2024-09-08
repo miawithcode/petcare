900853d059f24c5d5d4ffe62fdea0b0cc1856ae0
miawithcode <cuttontail@gmail.com>
2024-09-08 22:05:07 +0800

    feat: add account page

--- src/app/(app)/app/account/page.tsx
+++ src/app/(app)/app/account/page.tsx
@@ -1,3 +1,13 @@
+import ContentBlock from '@/components/content-block';
+import { Button } from '@/components/ui/button';
+
 export default function Page() {
-  return <div>Account page</div>;
+  return (
+    <ContentBlock className="flex flex-col items-center justify-center gap-2">
+      <p>
+        Logged in as <span className="font-semibold">example@gmail.com</span>
+      </p>
+      <Button>Logout</Button>
+    </ContentBlock>
+  );
 }
--- src/components/content-block.tsx
+++ src/components/content-block.tsx
@@ -1,10 +1,19 @@
+import { cn } from '@/lib/utils';
+
 export default function ContentBlock({
   children,
+  className,
 }: {
   children: React.ReactNode;
+  className?: string;
 }) {
   return (
-    <div className="bg-white h-full w-full overflow-hidden rounded-lg shadow-sm">
+    <div
+      className={cn(
+        'h-full w-full overflow-hidden rounded-lg bg-white shadow-sm',
+        className,
+      )}
+    >
       {children}
     </div>
   );
